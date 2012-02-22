3c3370cd31834614dd0de27a316d81adb918737f
Roy de Kleijn <rdekleijn@xmsnet.nl>
2012-02-22 22:16:24 +0100

    added more examples

---
+++ src/main/java/IfStatements.java
@@ -0,0 +1,16 @@
+
+public class IfStatements {
+	
+	public static void main(String[] args) {
+		
+		if("test".equals("test")) {
+			System.out.println("Both text strings are equal.");
+		}
+		
+		if(!"sometext".equals("test")) {
+			System.out.println("Both text strings are not equal!");
+		}
+		
+	}
+
+}
